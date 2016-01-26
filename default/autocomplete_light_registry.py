d117f29440ace8ad883ac092b68154ffb2e88537
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2016-01-26 12:24:31 -0200

    Se filtra por la sección del material al retornar valores posibles para la familia durante el autocompletado.

--- default/autocomplete_light_registry.py
+++ default/autocomplete_light_registry.py
@@ -1,28 +1,15 @@
 #encoding=utf8
 
 import autocomplete_light.shortcuts as al
-from models import Familia
+from models import Familia, Material
 
-# This will generate a PersonAutocomplete class.
-al.register(
-    Familia,
-    # Just like in ModelAdmin.search_fields.
-    search_fields=['secao__secao', 'grupo', 'subgrupo', 'familia', 'cod_familia'],
-    attrs={
-        # This will set the input placeholder attribute:
-        'placeholder': 'Other model name ?',
-        # This will set the yourlabs.Autocomplete.minimumCharacters
-        # options, the naming conversion is handled by jQuery.
-        'data-autocomplete-minimum-characters': 2,
-        },
-    # This will set the data-widget-maximum-values attribute on the
-    # widget container element, and will be set to
-    # yourlabs.Widget.maximumValues (jQuery handles the naming
-    # conversion).
-    widget_attrs={
-        'data-widget-maximum-values': 10,
-        # Enable modern-style widget !
-        'class': 'modern-style',
-        },
-    )
+class FamiliaAutocomplete(al.AutocompleteModelBase):
+    search_fields = ['secao__secao', 'grupo', 'subgrupo', 'familia', 'cod_familia']
+    model = Familia
 
+    def choices_for_request(self):
+        material = Material.objects.get(cod_material = self.request.GET['cod_material'])
+        self.choices = self.choices.filter(secao__id=material.secao.id)
+        return super(FamiliaAutocomplete, self).choices_for_request()
+
+al.register(FamiliaAutocomplete)
