083e82b5643f979511bf2a95d46c4ee50f1ea275
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2016-03-14 13:35:32 -0300

    Comandos para exportar herarquia y materiles con su familia.

---
+++ default/management/commands/expfam.py
@@ -0,0 +1,43 @@
+#encoding=utf8
+from django.core.management.base import BaseCommand, CommandError
+from default.models import *
+from multiprocessing import Pool
+from els.utils import ElasticFilesGenerator
+
+class Command(BaseCommand):
+    help = 'Gera um arquivo de familias separado por tabuladores.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('cod_secao', nargs='*')
+
+    def handle(self, *args, **options):
+
+        if len(options['cod_secao']) == 0:
+            familias = Familia.objects.all()
+        else:
+            familias = Familia.objects.filter(secao__cod_secao__in=options['cod_secao'])
+
+        print((u"%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+            u"Setor",
+            u"Cod. Seção",
+            u"Seção",
+            u"Cod. Grupo",
+            u"Grupo",
+            u"Cod. Subgrupo",
+            u"Subgrupo",
+            u"Cod. Familia",
+            u"Familia",
+            )).encode('utf8'))
+        for f in familias:
+            print((u"%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+                f.secao.setor.setor,
+                f.secao.cod_secao,
+                f.secao.secao,
+                f.cod_grupo,
+                f.grupo,
+                f.cod_subgrupo,
+                f.subgrupo,
+                f.cod_familia,
+                f.familia,
+                )).encode('utf8'))
+
---
+++ default/management/commands/expmat.py
@@ -0,0 +1,32 @@
+#encoding=utf8
+from django.core.management.base import BaseCommand, CommandError
+from default.models import *
+from multiprocessing import Pool
+from els.utils import ElasticFilesGenerator
+
+class Command(BaseCommand):
+    help = 'Gera um arquivo de familias separado por tabuladores.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('cod_secao', nargs='*')
+
+    def handle(self, *args, **options):
+
+        if len(options['cod_secao']) == 0:
+            materiais = Material.objects.all()
+        else:
+            materiais = Material.objects.filter(secoes_possiveis__cod_secao__in=options['cod_secao'])
+
+
+        print((u"%s\t%s" % (
+            u"Cod. Material",
+            u"Cod. Familia",
+            )).encode('utf8'))
+
+        for m in materiais:
+            print((u"%s\t%s" % (
+                m.cod_material,
+                m.familia.cod_familia,
+                )).encode('utf8'))
+
+
